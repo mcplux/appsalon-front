f3cb470b6045071063e4d68bd803b51801b9385b
Juan Martinez <pablomar01@outlook.com>
2024-07-01 15:55:42 -0500

    chore: update page layout to include a sidebar with background image

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,11 @@
 
 <template>
 
-  <RouterView />
+  <div class="md:flex h-screen">
+    <div class="h-64 md:h-auto bg-cover bg-center md:w-1/3 bg-[url('/img/1.jpg')]"></div>
+
+    <div class="md:w-2/3 px-10 py-5 min-h-full md:overflow-y-scroll">
+      <RouterView />
+    </div>
+  </div>
 </template>
