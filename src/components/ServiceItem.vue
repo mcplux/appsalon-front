d758a9045d9dacd5ef24c533c366b95baa446192
Juan Martinez <pablomar01@outlook.com>
2024-07-02 10:19:47 -0500

    chore: Add ServiceItem component and Appointments store

--- src/components/ServiceItem.vue
+++ src/components/ServiceItem.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { formatCurrency } from '@/helpers'
+import { useAppointmentsStore } from '@/stores/appointments'
 
 defineProps({
   service: {
@@ -7,11 +8,25 @@ defineProps({
     required: true,
   }
 })
+
+const appointmentsStore = useAppointmentsStore()
 </script>
 
 <template>
-  <div class="p-5 space-y-5 rounded-lg cursor-pointer bg-white">
-    <p class="text-2xl font-light">{{ service.name }}</p>
-    <p class="text-4xl font-black text-blue-600">{{ formatCurrency(service.price) }}</p>
+  <div 
+    class="p-5 space-y-5 rounded-lg cursor-pointer" 
+    :class="[ appointmentsStore.isServiceSelected(service.id) ? 'bg-blue-500 text-white' : 'bg-white' ]"
+    @click="appointmentsStore.onServiceSelected(service)"
+  >
+    <p class="text-2xl font-light">
+      {{ service.name }}
+    </p>
+
+    <p 
+      class="text-4xl font-black" 
+      :class="[ appointmentsStore.isServiceSelected(service.id) ? 'text-white' : 'text-blue-600' ]"
+    >
+      {{ formatCurrency(service.price) }}
+    </p>
   </div>
 </template>
---
+++ src/stores/appointments.ts
@@ -0,0 +1,28 @@
+import { computed, ref, type ComputedRef, type Ref } from 'vue'
+import { defineStore } from 'pinia'
+import type { Service } from '@/types'
+
+export const useAppointmentsStore = defineStore('appointments', () => {
+  const services:Ref<Service[]> = ref([])
+  
+  function onServiceSelected(service:Service):void {
+    if(services.value.some(selectedService => selectedService.id === service.id)) {
+      services.value = services.value.filter(selectedService => selectedService.id !== service.id)
+      return
+    }
+    if(services.value.length >= 2) {
+      alert('You can only select up to 2 services')
+      return
+    }
+    services.value.push(service)
+  }
+
+  const isServiceSelected:ComputedRef<(id:number) => boolean> = computed(() => {
+    return id => services.value.some(service => service.id === id)
+  })
+
+  return {
+    onServiceSelected,
+    isServiceSelected,
+  }
+})
