697c1fac0ac999a7f52416c849c0697a05248ce6
Juan Martinez <pablomar01@outlook.com>
2024-07-01 20:00:14 -0500

    create appointmnets layout

--- src/views/Appointments/AppointmentsLayout.vue
+++ src/views/Appointments/AppointmentsLayout.vue
@@ -3,7 +3,28 @@
 </script>
 
 <template>
-  <h1>Appointments</h1>
+  <div class="flex justify-between">
+    <h1 class="text-2xl lg:text-6xl font-black text-white">AppSalon</h1>
+
+    <div class="flex flex-col space-y-5">
+      <div class="flex justify-end gap-2 items-center">
+        <p class="text-white text-right">Hi, user</p>
+        <button type="button" class="bg-red-600 hover:bg-red-700 uppercase p-2 text-white transition-colors rounded text-xs font-bold">
+          Sign out
+        </button>
+      </div>
+
+      <nav class="flex flex-col md:flex-row gap-2">
+        <button class="p-2 text-gray-200 uppercase text-xs font-black">
+          My appointmnets
+        </button>
+
+        <RouterLink :to="{ name: 'new-appointment' }" class="p-2 text-gray-200 bg-blue-600 uppercase text-xs font-black rounded">
+          New Appointmnet
+        </RouterLink>
+      </nav>
+    </div>
+  </div>
 
   <RouterView />
 </template>
