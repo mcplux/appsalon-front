593624332af8779a910a43bd1b669deeacce4e21
Juan Martinez <pablomar01@outlook.com>
2024-07-01 21:31:52 -0500

    make the request to the api for the services

---
+++ src/stores/services.ts
@@ -0,0 +1,27 @@
+import { onMounted, ref, type Ref } from 'vue'
+import { defineStore } from 'pinia'
+import servicesAPI from '@/api/servicesAPI'
+
+interface Service {
+  id: number
+  name: string
+  price: string
+}
+
+export const useServicesStore = defineStore('services', () => {
+  const services:Ref<Service[]> = ref([])
+
+  onMounted(async () => {
+    try {
+      const { data } = await servicesAPI.all()
+      services.value = data
+      console.log(services.value)
+    } catch (error) {
+      console.error(error)
+    }
+  })
+
+  return {
+    services,
+  }
+})
