d0c92a43595ff107a16a4ff84f2c534ee2ec782d
Juan Martinez <pablomar01@outlook.com>
2024-07-03 19:38:54 -0500

    chore: Update AuthLayout.vue to include authentication routes and navigation

--- src/views/Auth/AuthLayout.vue
+++ src/views/Auth/AuthLayout.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-
+const authRoutes = [
+  { name: 'login', text: 'Log in your account' },
+  { name: 'register', text: 'Create an account' },
+]
 </script>
 
 <template>
-  <h1>Auth</h1>
+  <div class="mx-auto lg:w-4/5 mt-20">
+    <RouterView />
 
-  <RouterView />
+    <nav class="mt-10 flex flex-col items-center space-y-5 lg:flex-row lg:justify-between lg:space-y-0">
+      <RouterLink 
+        v-for="route in authRoutes"
+        :key="route.name"
+        :to="route.name"
+        class="uppercase font-bold text-white"
+      >
+        {{ route.text }}
+      </RouterLink>
+    </nav>
+  </div>
 </template>
