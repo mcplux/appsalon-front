1f298ac24ca362cbacf81b2656d31bfedc104696
Juan Martinez <pablomar01@outlook.com>
2024-07-03 20:36:47 -0500

    chore: Update formkit.config.ts with generated classes for form styling and create register form in RegisterView.vue

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,6 +1,18 @@
+import { generateClasses } from "@formkit/themes"
+
 const config = {
   config: {
-
+    classes: generateClasses({
+      global: {
+        wrapper: 'space-y-2 mb-3',
+        message: 'bg-red-500 text-white p-2 text-center text-sm rounded font-bold my-2',
+        label: 'block mb-1 font-bold text-lg text-white',
+        input: 'w-full p-3 border border-gray-300 rounded text-gray-700 placeholder-gray-500',
+      },
+      submit: {
+        input: '$reset bg-blue-500 hover:bg-blue-600 transition-colors p-3 text-white font-bold rounded w-full mt-10',
+      }
+    })
   }
 }
 
--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -1,8 +1,55 @@
 <script setup lang="ts">
+interface RegisterForm {
+  first_name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+}
 
+const handleSubmit = ({ password_confirm, ...data }:RegisterForm) => {
+  console.log(data)
+};
 </script>
 
 <template>
   <h1 class="text-6xl font-extrabold text-white text-center">Register</h1>
   <p class="text-2xl text-white text-center mt-5">Create an account on AppSalon</p>
+
+  <FormKit type="form" :actions="false" @submit="handleSubmit">
+    <FormKit 
+      type="text"
+      label="Name"
+      name="first_name"
+      placeholder="Enter your name"
+      validation="required|length:3"
+    />
+
+    <FormKit
+      type="email"
+      label="Email"
+      name="email"
+      placeholder="Enter your email"
+      validation="required|email"
+    />
+
+    <FormKit
+      type="password"
+      label="Password"
+      name="password"
+      placeholder="Create a password - Min 8 characters"
+      validation="required|length:8"
+    />
+
+    <FormKit
+      type="password"
+      label="Confirm Password"
+      name="password_confirm"
+      placeholder="Confirm your password"
+      validation="required|confirm"
+    />
+
+    <FormKit type="submit">
+      Create account
+    </FormKit>
+  </FormKit>
 </template>
