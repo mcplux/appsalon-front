57f2408750dfa9cc06e15930106d979b02c1eada
Juan Martinez <pablomar01@outlook.com>
2024-07-03 20:07:17 -0500

    chore: Update formkit.config.ts and package.json with @formkit/themes and @formkit/vue dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { plugin, defaultConfig } from '@formkit/vue'
+
+import config from '../formkit.config'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +12,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(plugin, defaultConfig(config))
 app.use(router)
 
 app.mount('#app')
--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -3,5 +3,6 @@
 </script>
 
 <template>
-  <h1>Register</h1>
+  <h1 class="text-6xl font-extrabold text-white text-center">Register</h1>
+  <p class="text-2xl text-white text-center mt-5">Create an account on AppSalon</p>
 </template>
