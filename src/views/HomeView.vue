1e3e6c5e20d1a7326102214266128e99193027b9
Juan Martinez <pablomar01@outlook.com>
2024-07-18 16:01:50 -0500

    fix: Redirect user to appointments when visits /

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  router.push({ name: 'my-appointments' })
+})
 </script>
 
 <template>
